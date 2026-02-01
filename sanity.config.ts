79f564b2a55f0073bcfffcc4d5fd9e7c796c2dfc
Joyce Lin <ohanalin@gmail.com>
2026-02-01 14:56:45 -0500

    sanity for versal update

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
-import { visionTool } from '@sanity/vision'
 import { schema } from './src/sanity/schemaTypes'
 
 export default defineConfig({
@@ -9,6 +8,6 @@ export default defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   basePath: '/studio',
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool()],
   schema,
 })
